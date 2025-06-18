29048a1d7df2e2a317cd11a5244404641d8286ec
Mespyr <senukcw@gmail.com>
2025-06-17 23:15:58 -0400

    start learning the inner workings of TagLib

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ HEADERS=$(foreach dir, $(SRC_DIRS), $(wildcard $(dir)/*.hpp))
 
 format: $(SRC_FILES) $(HEADERS)
 	clang-format -i $(SRC_FILES) $(HEADERS) -style=file
+
+build: $(SRC_FILES) $(HEADERS)
+	cmake --build build/ -j$(nproc)
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,73 @@
+#include <taglib/fileref.h>
+
+#include <fstream>
+
 #include "util.hpp"
 #include "window/window.hpp"
 
-#include <taglib/fileref.h>
+bool extractAlbumCoverRGBA(const std::string          &file_path,
+                           std::vector<unsigned char> &rgbaPixels, int &width,
+                           int &height) {
+    // Open file using FileRef
+    TagLib::FileRef fileRef(file_path.c_str());
+    if (fileRef.isNull()) {
+        std::cerr << "Error: Cannot open file or unsupported format.\n";
+        return false;
+    }
+    TagLib::List<TagLib::VariantMap> props =
+        fileRef.complexProperties("PICTURE");
+    if (props.isEmpty()) {
+        std::cerr << "Error: No embedded picture found.\n";
+        return false;
+    }
+    std::cout << fileRef.tag()->album() << std::endl;
+    std::cout << fileRef.tag()->artist() << std::endl;
+    std::cout << fileRef.tag()->title() << std::endl;
+    for (const TagLib::VariantMap prop : props) {
+        TagLib::Variant value = prop.value("data");
+        if (!value.isEmpty() && value.type() == TagLib::Variant::ByteVector) {
+            TagLib::ByteVector raw_data = value.value<TagLib::ByteVector>();
+            std::ofstream      picture;
+            TagLib::String     fn("extract");
+            int                slashPos = fn.rfind('/');
+            int                dotPos = fn.rfind('.');
+            if (slashPos >= 0 && dotPos > slashPos) {
+                fn = fn.substr(slashPos + 1, dotPos - slashPos - 1);
+            }
+            fn += ".jpg";
+            picture.open(fn.toCString(),
+                         std::ios_base::out | std::ios_base::binary);
+            picture << raw_data;
+            picture.close();
+        }
+    }
+
+    return true;
+}
 
 int main() {
-    std::ostream& log = std::cerr;
-    if (!init_lib(log)) exit(1);
+    int                        width, height;
+    std::vector<unsigned char> pixels;
+    extractAlbumCoverRGBA("music/IGOR'S THEME.mp3", pixels, width, height);
+    extractAlbumCoverRGBA("music/Let It Happen.mp3", pixels, width, height);
+    extractAlbumCoverRGBA("music/Lost In Yesterday.mp3", pixels, width, height);
+    extractAlbumCoverRGBA("music/St. Chroma.mp3", pixels, width, height);
+    extractAlbumCoverRGBA("music/Thought I Was Dead.mp3", pixels, width,
+                          height);
+    extractAlbumCoverRGBA("music/WHAT'S GOOD.mp3", pixels, width, height);
 
-    Window window(log);
-    if (!window) exit(1);
+    // std::ostream &log = std::cerr;
+    // if (!init_lib(log)) exit(1);
 
-    do {
-        window.update_events();
-        window.clear();
-        window.render();
-    } while (window);
+    // Window window(log);
+    // if (!window) exit(1);
 
-    SDL_Quit();
+    // do {
+    //     window.update_events();
+    //     window.clear();
+    //     window.render();
+    // } while (window);
+
+    // SDL_Quit();
     return 0;
 }
